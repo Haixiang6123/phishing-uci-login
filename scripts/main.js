465243388fe2295b8992c3aca628885c9831c546
Haixiang6123 <haixiang6123@gmail.com>
2019-07-07 16:52:54 -0700

    Add evil script

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const loginButton = document.querySelector('#login-button')
+
+loginButton.addEventListener('click', function (event) {
+    const loginStatus = document.querySelector('#login_status_span')
+    const loginImage = document.querySelector('#login_image_span')
+    loginButton.style.display = 'none'
+    loginStatus.style.visibility = 'visible'
+    loginImage.style.visibility = 'visible'
+    setTimeout(function () {
+        alert('You hare hacked, haha !!!')
+    }, 1000)
+})
